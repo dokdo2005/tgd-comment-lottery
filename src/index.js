6b26859b4b218421851da9ff38903a00997605e9
Gary Lee <dokdo2005@gmail.com>
2022-04-01 20:07:28 +0900

    chore: upgrade to React 18

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/index.css';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./css/index.css";
+import App from "./App";
 
 // Importing the Bootstrap CSS
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
-
